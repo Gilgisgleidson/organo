c7ebaf8054cf3e8fa4fbd80c7408f65950700cbe
Gilgisgleidson <gilgisgleidson@gmail.com>
2024-07-19 17:05:20 -0300

    Mudanças

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,13 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div>
-                <a href="facebook.com/organo" target="_blank">
+                <a href="https://facebook.com" target="_blank" rel="noreferrer">
                     <img src='/images/fb.png' alt='face' />
                 </a>
-                <a href="twitter.com/organo" target="_blank">
+                <a href="https://twitter.com" target="_blank" rel="noreferrer">
                     <img src='/images/tw.png' alt='X'/>
                 </a>
-                <a href="instagram.com/organo" target="_blank">
+                <a href="https://instagram.com" target="_blank" rel="noreferrer">
                     <img src='/images/ig.png' alt='insta'/>
                 </a>
             </div>
